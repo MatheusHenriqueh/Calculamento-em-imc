f65df94c1da79cfb7d7359cd4a1c8592271248e4
Matheus Ruivo <matheushenriquerodriguesruivo4@gmail.com>
2025-10-08 22:18:40 -0300

    Update imc.py

--- imc.py
+++ imc.py
@@ -1,7 +1,9 @@
 #Projeto de calculamento de IMC (Indice de Massa Muscular)
+#Autor: Matheus Ruivo
 
 name = input('Qual é seu nome?')
 altura = float(input('Qual é sua altura atualmente em metros?'))
 peso = float(input("Qual é seu peso (kg)?"))
 imc = peso / (altura ** 2)
-print(name, "você tem", altura,"e pesa", peso,"seu imc é", imc)
+ # print(name, "você tem", altura,"e pesa", peso,"seu imc é", imc)
+print(f"{name} você tem {altura} metros e pesa {peso} seu imc é {imc}") #Evolução
